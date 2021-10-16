ed5224975f99ee86f70dc18f736c324f43d77c61
george-shammar <george.gbenle@outlook.com>
2021-10-16 18:54:36 +0000

    Implement COunters

--- contracts/Protokol.sol
+++ contracts/Protokol.sol
@@ -4,11 +4,14 @@ pragma solidity ^0.8.0;
 import "hardhat/console.sol";
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/security/Pausable.sol";
-import "@openzeppelin/contracts/access/Ownable.sol"
+import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 
 contract Protokol is ERC721, Pausable, Ownable {
     
+    using Counters for Counters.Counter;
+    using Strings for uint256;
+    Counters.Counter _tokenIds;
 
     constructor()ERC721("Protokol", "PTK"){
 
