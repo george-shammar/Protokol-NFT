f30f83ea5df5f93145f0960e07cea241eabb2449
george-shammar <george.gbenle@outlook.com>
2021-10-16 23:18:01 +0000

    Import opezeppelin's ERC721Storage contract

--- contracts/Protokol.sol
+++ contracts/Protokol.sol
@@ -4,6 +4,7 @@ pragma solidity ^0.8.0;
 
 import "hardhat/console.sol";
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/security/Pausable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
