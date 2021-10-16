4f03bb049153518d0f955ca231ca7bb7513c6f12
george-shammar <george.gbenle@outlook.com>
2021-10-16 18:43:23 +0000

    Import library

--- contracts/Protokol.sol
+++ contracts/Protokol.sol
@@ -3,6 +3,9 @@ pragma solidity ^0.8.0;
 
 import "hardhat/console.sol";
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/security/Pausable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol"
+import "@openzeppelin/contracts/utils/Counters.sol";
 
 contract Protokol {
 
