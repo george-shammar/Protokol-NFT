f8c7cc2c5e5a0f8d17ffacf2dfcb13c2507ad05b
george-shammar <george.gbenle@outlook.com>
2021-10-28 20:27:53 +0000

    Remove Max token limit

--- contracts/Protokol.sol
+++ contracts/Protokol.sol
@@ -16,7 +16,7 @@ contract Protokol is ERC721, Pausable, Ownable, ERC721URIStorage {
     using Counters for Counters.Counter;
     using Strings for uint256;
     Counters.Counter private _tokenIds;
-    uint256 public constant MAX_TOKEN = 1000;
+   
 
     /// @dev Sets maximum number of mintable tokens at once.
     uint256 public maxMintAmount = 10;
